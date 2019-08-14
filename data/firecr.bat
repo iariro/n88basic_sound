69a91bddc2c3943c907ef87c6b430ef992f92f2a
kumagai <iariro@gol.com>
2019-08-14 17:35:10 +0900

    new

---
+++ data/firecr.bat
@@ -0,0 +1,12 @@
+
+COM=com6
+
+copy fire1.mml %COM%
+timeout /t 13
+copy fire1.mml %COM%
+timeout /t 13
+copy fire2.mml %COM%
+timeout /t 16
+copy fire3.mml %COM%
+timeout /t 16
+copy fire4.mml %COM%
