33a5dcf0406e52ce97e1d5a2adf13807105ee9b3
kumagai <iariro@gol.com>
2019-08-15 12:43:28 +0900

    イントロ追加

--- data/firecr.bat
+++ data/firecr.bat
@@ -4,22 +4,24 @@ set COM=com6
 copy fire0.mml %COM%
 timeout /t 8
 copy fire1.mml %COM%
-timeout /t 13
-copy fire1.mml %COM%
+timeout /t 8
+copy fire2.mml %COM%
 timeout /t 13
 copy fire2.mml %COM%
-timeout /t 16
-copy fire3.mml %COM%
+timeout /t 13
+copy fire3-1.mml %COM%
 timeout /t 16
 copy fire4.mml %COM%
 timeout /t 16
-
-copy fire1.mml %COM%
-timeout /t 13
-copy fire1.mml %COM%
-timeout /t 13
 copy fire5.mml %COM%
 timeout /t 16
-copy fire3.mml %COM%
+
+copy fire2.mml %COM%
+timeout /t 13
+copy fire2.mml %COM%
+timeout /t 13
+copy fire3-2.mml %COM%
 timeout /t 16
 copy fire4.mml %COM%
+timeout /t 16
+copy fire5.mml %COM%
