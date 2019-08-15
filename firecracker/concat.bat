440c6b718c592e5b8ca5fb1306c7920b774c977e
kumagai <iariro@gol.com>
2019-08-15 20:18:58 +0900

    コーダ仕上げ

--- firecracker/concat.bat
+++ firecracker/concat.bat
@@ -1,2 +1,2 @@
 
-type fire1.mml fire2.mml fire2.mml fire3-1.mml fire4-1.mml fire5.mml fire2.mml fire2.mml fire3-2.mml fire4-2.mml fire5.mml > a:firecr.mml
+type fire1.mml fire2.mml fire2.mml fire3-1.mml fire4-1.mml fire5.mml fire2.mml fire2.mml fire3-2.mml fire4-2.mml fire5.mml fire6.mml fire7.mml fire8.mml > a:firecr.mml
