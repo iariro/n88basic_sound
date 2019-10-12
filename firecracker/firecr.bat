dee017883d6c1e6dac1c83d7e12eeba013931cc0
kumagai <iariro@gol.com>
2019-10-12 11:03:34 +0900

    ピコピコ音改善・ピアノソロ音変える・最後の２拍交代音変える

--- firecracker/firecr.bat
+++ firecracker/firecr.bat
@@ -1,6 +1,8 @@
 
 set COM=com6
 
+copy fire0.mml %COM%
+timeout /t 8
 copy fire1.mml %COM%
 timeout /t 8
 copy fire2.mml %COM%
@@ -23,3 +25,9 @@ timeout /t 16
 copy fire4-2.mml %COM%
 timeout /t 16
 copy fire5.mml %COM%
+timeout /t 16
+copy fire6.mml %COM%
+timeout /t 16
+copy fire7.mml %COM%
+timeout /t 16
+copy fire8.mml %COM%
