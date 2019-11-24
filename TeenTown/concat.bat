0a7385b1367e9fed0d35e3ec69e7fe224c4c039d
kumagai <iariro@gol.com>
2019-11-24 09:23:00 +0900

    続き入力

---
+++ TeenTown/concat.bat
@@ -0,0 +1,2 @@
+
+type teen01.mml teen02.mml teen03.mml teen04.mml teen05.mml > a:teen.mml
--- TeenTown/teen.bat
+++ TeenTown/teen.bat
@@ -1,16 +1,17 @@
 
 set COM=com6
+set WAIT=5
 
 copy teen01.mml %COM%
-timeout /t 5
+timeout /t %WAIT%
 
 copy teen02.mml %COM%
-timeout /t 5
+timeout /t %WAIT%
 
 copy teen03.mml %COM%
-timeout /t 5
+timeout /t %WAIT%
 
 copy teen04.mml %COM%
-timeout /t 5
+timeout /t %WAIT%
 
 copy teen05.mml %COM%
