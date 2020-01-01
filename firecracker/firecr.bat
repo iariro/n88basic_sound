c08803395497bf677e251af1b47d52e80d40eca2
kumagai <iariro@gol.com>
2020-01-01 19:20:28 +0900

    タイミング調整

--- firecracker/firecr.bat
+++ firecracker/firecr.bat
@@ -2,32 +2,32 @@
 set COM=com6
 
 copy fire0.mml %COM%
-timeout /t 8
+timeout /t 5
 copy fire1.mml %COM%
-timeout /t 8
+timeout /t 5
 copy fire2.mml %COM%
-timeout /t 13
+timeout /t 10
 copy fire2.mml %COM%
-timeout /t 13
+timeout /t 10
 copy fire3-1.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire4-1.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire5.mml %COM%
-timeout /t 16
+timeout /t 10
 
 copy fire2.mml %COM%
-timeout /t 13
+timeout /t 10
 copy fire2.mml %COM%
-timeout /t 13
+timeout /t 10
 copy fire3-2.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire4-2.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire5.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire6.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire7.mml %COM%
-timeout /t 16
+timeout /t 10
 copy fire8.mml %COM%
